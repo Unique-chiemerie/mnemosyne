9aa5c24e6016fd7d2499c7c5c33f6a9d86117c7c
Unique-chiemerie <lilanimate1@gmail.com>
2024-06-07 21:01:51 +0100

    building place holders for the homescreen

--- lib/Screens/mainscreen.dart
+++ lib/Screens/mainscreen.dart
@@ -33,12 +33,11 @@ class Mainscreen extends StatelessWidget {
         ],
       ),
       body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
         children: [
           //the placeholder for the quotes card
           Container(
-            height: screenHeight * 0.3,
-            width: screenWidth * 0.7,
+            height: screenHeight * 0.1,
+            width: screenWidth * 0.75,
             decoration: BoxDecoration(
               borderRadius: BorderRadius.circular(10),
               color: Colors.lightBlue,
@@ -49,8 +48,8 @@ class Mainscreen extends StatelessWidget {
           ),
           //the place holder for the random animated character
           SizedBox(
-            height: screenHeight * 0.5,
-            width: screenWidth * 0.5,
+            height: screenHeight * 0.4,
+            width: screenWidth * 0.8,
             child: const Placeholder(
               color: Colors.red,
             ),
